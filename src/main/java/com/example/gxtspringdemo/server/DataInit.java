49b373ae0d71d0361dfddcb7605b289284cad4ac
Valery Kiryanov <VIKiryanov.SBT@sberbank.ru>
2018-08-24 17:50:58 +0300

    add H2 support

---
+++ src/main/java/com/example/gxtspringdemo/server/DataInit.java
@@ -0,0 +1,14 @@
+package com.example.gxtspringdemo.server;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInit implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
