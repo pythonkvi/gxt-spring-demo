a8898dc3ea18d4c7627bb9d9f405e70d087766ab
Valery Kiryanov <VIKiryanov.SBT@sberbank.ru>
2018-08-24 16:09:35 +0300

    add sample app

---
+++ src/main/java/com/example/gxtspringdemo/GxtSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.gxtspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GxtSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GxtSpringDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/gxtspringdemo/GxtSpringDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.gxtspringdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GxtSpringDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
